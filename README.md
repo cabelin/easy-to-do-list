48f1ecc0cef18ac0f96a1238a398521af9302993
Mateus Moraes <mmds89@gmail.com>
2019-10-07 22:51:25 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+## Easy To-Do List
+
+Esse projeto é um To-Do List simples.
+
+É um exemplo prático de React + Typescript + StyledComponents + afins
---
+++ src/Assets/Theme/index.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: '#1a237e',
+    secondary: '#3f51b5',
+  },
+};
+
+export { myTheme };
